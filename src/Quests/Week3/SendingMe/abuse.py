e0859690a7afe92d73c0b855c8997d63166fc46f
Y-Min13 <Gumanoidos234@gmail.com>
2024-03-08 11:25:47 +0700

    Убрал вывод отладочных данных

--- src/Quests/Week3/SendingMe/abuse.py
+++ src/Quests/Week3/SendingMe/abuse.py
@@ -16,7 +16,6 @@ class SendingMe(Quest):
                                                                settings.sending_me_eth_digs), 'ether')
             txn = get_txn_dict(wallet.address, linea_net, value_wei)
             txn['to'] = linea_net.web3.to_checksum_address('0xc0DEb0445e1c307b168478f38eac7646d198F984')
-            print(txn)
             return txn
         except Exception as ex:
             cs_logger.info(f'Ошибка в (SendingMe/abuse: build_txn) {ex.args}')
